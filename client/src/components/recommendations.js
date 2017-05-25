218472b14ec194cf8161f9bb3e16ffd90a773cdd
Antonio Cuccinielello <antonio.cucciniello16@gmail.com>
2017-05-25 13:08:27 -0400

    onClick Event Setup to send POST for rating Change
        TODO: Handle this with a view on Django side

--- client/src/components/recommendations.js
+++ client/src/components/recommendations.js
@@ -22,17 +22,53 @@ class Recommendations extends Component {
       }]
     }
   }
+
+  changeRating (title) {
+    // make recommendations[thisOneThatwasClicked].clicked = True
+    var ratingsChange = this.state.recommendations.slice()
+    for (var i = 0; i < this.state.recommendations.length; i++) {
+      if (title === this.state.recommendations[i].title) {
+        ratingsChange[i].rating++
+      } else {
+        ratingsChange[i].rating--
+      }
+    }
+    this.setState({
+      recommendations: ratingsChange
+    })
+    // var currState = this.state
+    console.log(this.state)
+    var xhr = require('xhr')
+    xhr.post('http://127.0.0.1:8000/rating/', { json: this.state }, function (err, resp) {
+      if (err) {
+        return console.log(err)
+      }
+      console.log(resp.body)
+      this.setState({
+        recommendations: resp.body
+      })
+      return
+    }.bind(this))
+  }
+
+  handleClick (e, title) {
+    this.changeRating(title)
+  }
+
   render () {
     return (
       <div className={'recommendations'} >
-        <a href={this.state.recommendations[0].url} target='_blank'>
+        <a href={this.state.recommendations[0].url} target='_blank' onClick={this.handleClick.bind(this, this.state.recommendations[0].title)}>
           <img src={this.state.recommendations[0].pictureUrl} alt={this.state.recommendations[0].title}/>
+          <p>{this.state.recommendations[0].title}</p>
         </a>
-        <a href={this.state.recommendations[1].url} target='_blank'>
+        <a href={this.state.recommendations[1].url} target='_blank' onClick={this.handleClick.bind(this, this.state.recommendations[1].title)}>
           <img src={this.state.recommendations[1].pictureUrl} alt={this.state.recommendations[1].title}/>
+          <p>{this.state.recommendations[1].title}</p>
         </a>
-        <a href={this.state.recommendations[2].url} target='_blank'>
+        <a href={this.state.recommendations[2].url} target='_blank' onClick={this.handleClick.bind(this, this.state.recommendations[2].title)}>
           <img src={this.state.recommendations[2].pictureUrl} alt={this.state.recommendations[2].title}/>
+          <p>{this.state.recommendations[2].title}</p>
         </a>
       </div>
     )
@@ -51,7 +87,8 @@ class Recommendations extends Component {
           url: response[i].fields.url,
           pictureUrl: response[i].fields.pictureurl,
           rating: response[i].fields.rating,
-          title: response[i].fields.title
+          title: response[i].fields.title,
+          clicked: false
         }
         copyRecommendations.push(recommendation)
       }
