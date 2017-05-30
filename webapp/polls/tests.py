5423d323427e4fed13fb75025926455e3ec333b0
Antonio Cuccinielello <antonio.cucciniello16@gmail.com>
2017-05-30 18:36:35 -0400

    Tests for Recommendation Model implemented
        TODO:
            Test the three views
            Then tests for react

--- webapp/polls/tests.py
+++ webapp/polls/tests.py
@@ -1,6 +1,19 @@
-# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.test import TestCase
-
+from .models import Recommendation
 # Create your tests here.
+
+class TestCaseRecommendation(TestCase):
+  def setUp(self):
+    self.recommendation = Recommendation.objects.create(title='Some Clothing', url='http://google.com', rating=0, pictureurl='http://picture.com')
+
+class TestRecommendationMethods(TestCaseRecommendation):
+  def test_init(self):
+    self.assertEqual(self.recommendation.title, 'Some Clothing')
+    self.assertEqual(self.recommendation.url, 'http://google.com')
+    self.assertEqual(self.recommendation.rating, 0)
+    self.assertEqual(self.recommendation.pictureurl, 'http://picture.com')
+
+  def test_str(self):
+    strOutput = str(self.recommendation)
+    self.assertEqual(strOutput, 'Some Clothing http://google.com http://picture.com 2 0 ')
